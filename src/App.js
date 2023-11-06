06acf80646692a298901fb764a26a83e28ee535f
mahshadEilanlou <ilanloumahshad@gmail.com>
2023-11-05 20:25:42 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [lineOne] = useState("Welcome to Fullstack Development 1");
+  const [lineTwo] = useState("React Js Programming Week9 lab exercise");
+  const [lineThree] = useState("my student Id : 101400600");
+  const [lineFour] = useState("Mahshad Eilanlou");
+  const [lineFive] = useState("George Brown College, Toronto");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{lineOne}</h1>
+        <h2>{lineTwo}</h2>
+        <h3>{lineThree}</h3>
+        <h4>{lineFour}</h4>
+        <p>{lineFive}</p>
       </header>
     </div>
   );
